c08a2c11b6431092b55fe601f1d96eee29a382da
MihaiChirculete <mihai.chirculete98@gmail.com>
2017-08-12 17:02:24 +0300

    Added print function for strings

--- drivers/screen.c
+++ drivers/screen.c
@@ -7,13 +7,28 @@ void kprint(char c, char attr, int x, int y)
 {
 	char *fb = (char *) 0x000B8000;
 
-	int offset = (y * MAX_COLS * 2 + x * 2);
+	int offset = (y * MAX_COLS + x) * 2;
 
 	fb[offset] = c;
 	fb[offset + 1] = attr;
 }
 
+void kprint_string(char msg[], char attr, int x, int y)
+{
+	while(*msg != '\0')
+	{
+		if(x>MAX_COLS)
+		{
+			x = 0;
+			y++;
+		}
 
+		kprint(*msg, attr, x, y);
+
+		x++;
+		msg++;
+	}
+}
 
 void clear_screen()
 {
@@ -22,4 +37,6 @@ void clear_screen()
 	for(i=0; i<MAX_COLS; i++)
 		for(j=0; j<MAX_ROWS; j++)
 			kprint(' ', WHITE_ON_BLACK, i, j);
+
+	fb_move_cursor(0x0000);
 }
